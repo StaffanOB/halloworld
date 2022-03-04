0ddff4b93b2fb5c52fd0117bed7e6a250d705062
staffan <staffan@toor.se>
2022-03-04 18:04:05 +0100

    release 0.0.2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
 		System.out.println("Hello, World!"); 
-		System.out.println("This is the development branch"); 
+		System.out.println("Building release 0.0.2"); 
     }
 }
